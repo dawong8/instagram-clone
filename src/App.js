d6438fe0f1ca7106bb6724651647f7d7ca4cfb52
Akshay199456 <akshay.kum94@gmail.com>
2019-02-10 08:06:21 -0800

    Added Router, Switch capability

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 import ParentLoginRegister from './containers/ParentLoginRegister';
+import { Route, Switch } from 'react-router-dom';
+
+const My404 = () =>{
+	return(
+		<div>
+			You Are Lost!!!
+		</div>
+	);
+}
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ParentLoginRegister/>
-      </div>
+      <main>
+      	<Switch>
+      		<Route exact path='/' component= { ParentLoginRegister } />
+      		<Route component= { My404 } />
+      	</Switch>
+      </main>
     );
   }
 }
