af38151876fe789d331cc3cbf47f7dc4159de879
Akshay199456 <akshay.kum94@gmail.com>
2019-02-11 11:59:42 -0800

    Refectored code; complete FB functionality; bugDiscovered: allows duplicate accounts to be created

--- src/containers/ParentLoginRegister/index.js
+++ src/containers/ParentLoginRegister/index.js
@@ -45,57 +45,13 @@ class ParentLoginRegister extends Component{
 
 
 	// Handles the login submit form when the button is clicked
-	handleLoginSubmit = async (e) =>{
+	handleLoginSubmit = (e) =>{
 		e.preventDefault();
 		const updatedLogin = {
 			...this.state.login //spreads current value of register into updatedRegister
 		}
 		
-		try{
-			const response = await fetch('http://localhost:9000/auth/login', {
-			method: 'POST',
-			credentials: 'include',
-			body: JSON.stringify(updatedLogin),
-			headers: {
-				'Content-Type': 'application/json'
-				}
-			});
-
-			console.log("Login response: ", response);
-
-			if(!response.ok){
-				throw Error(response.statusText);
-
-			}
-
-			const parsedReponse = await response.json();
-			console.log("Parsed response: ", parsedReponse);
-
-			if(parsedReponse.data === 'login successful')
-			// Sets login to successful if the user successfully logs into account
-			{
-				updatedLogin.errorMsg = '';
-				updatedLogin.successful = true;
-				this.setState({
-					login: updatedLogin
-				});
-				this.props.history.push('/home');
-			}
-
-			else{
-				updatedLogin.errorMsg = 'Email/Password incorrectly entered. Make sure you have registered an account and have entered the correct login details';
-
-				this.setState({
-					login: updatedLogin
-				});
-				this.props.history.push('/');
-			}
-			
-		}
-
-		catch(err){
-			console.log("Error: ", err);
-		}
+		this.fetchLogin(updatedLogin);
 	}
 
 	//
@@ -121,25 +77,9 @@ class ParentLoginRegister extends Component{
 		});
 	}
 
-	setStateFacebook = async (name, email, id, status) =>{
-		if(status === 'register')
-		{
-			this.setState({
-				register:{
-					username: name,
-					email: email,
-					password: id,
-					successful: false,
-					errorMsg: ''
-				}
-			});
 
-			const updatedRegister = {
-			...this.state.register //spreads current value of register into updatedRegister
-			}
-
-
-			try{
+	fetchRegister = async (updatedRegister) =>{
+		try{
 			const response = await fetch('http://localhost:9000/auth', {
 			method: 'POST',
 			credentials: 'include',
@@ -193,21 +133,10 @@ class ParentLoginRegister extends Component{
 		}
 	}
 
-		else{
-			this.setState({
-				login:{
-					username: name,
-					password: id,
-					successful: false,
-					errorMsg: ''
-				}
-			});
 
-			const updatedLogin = {
-			...this.state.login //spreads current value of register into updatedRegister
-		}
 
-			try{
+	fetchLogin = async (updatedLogin) =>{
+		try{
 				const response = await fetch('http://localhost:9000/auth/login', {
 				method: 'POST',
 				credentials: 'include',
@@ -252,69 +181,58 @@ class ParentLoginRegister extends Component{
 		catch(err){
 			console.log("Error: ", err);
 		}
+	}
+
+
+
+	setStateFacebook =  (name, email, id, status) =>{
+		if(status === 'register')
+		{
+			this.setState({
+				register:{
+					username: name,
+					email: email,
+					password: id,
+					successful: false,
+					errorMsg: ''
+				}
+			});
+
+			const updatedRegister = {
+			...this.state.register //spreads current value of register into updatedRegister
+			}
+
+
+			this.fetchRegister(updatedRegister);
+	}
+
+		else{
+			this.setState({
+				login:{
+					username: name,
+					password: id,
+					successful: false,
+					errorMsg: ''
+				}
+			});
+
+			const updatedLogin = {
+			...this.state.login //spreads current value of register into updatedRegister
+			}
+
+			this.fetchLogin(updatedLogin);
 		}
 	}
 
 
 	// Handles the register submit form when then the button is clicked
-	handleRegisterSubmit = async (e) =>{
+	handleRegisterSubmit = (e) =>{
 		e.preventDefault();
 		const updatedRegister = {
 			...this.state.register //spreads current value of register into updatedRegister
 		}
 		
-		try{
-			const response = await fetch('http://localhost:9000/auth', {
-			method: 'POST',
-			credentials: 'include',
-			body: JSON.stringify(updatedRegister),
-			headers: {
-				'Content-Type': 'application/json'
-				}
-			});
-
-			// console.log("Register response: ",response);
-
-			if(!response.ok){
-				throw Error(response.statusText);
-
-			}
-
-			const parsedReponse = await response.json();
-			console.log("Parsed response: ", parsedReponse);
-
-			if(parsedReponse.data === 'register successful')
-			// Sets register to successful if the user successfully registers an account
-			{
-				updatedRegister.errorMsg = '';
-				updatedRegister.successful = true;
-				this.setState({
-					register: updatedRegister
-				});
-				this.props.history.push('/home');
-			}
-
-			else{
-				if(parsedReponse.errmsg.includes('email')){
-					console.log("Email already exists");
-					updatedRegister.errorMsg = 'Email already exists. Please enter a new email';
-				}
-				else if(parsedReponse.errmsg.includes('username')){
-					console.log("Username already exists");
-					updatedRegister.errorMsg = 'Username already exists. Please enter a new username';
-				}
-
-				this.setState({
-					register: updatedRegister
-				});
-				this.props.history.push('/');
-			}
-			
-		}
-
-		catch(err){
-			console.log("Error: ", err);
-		}
+		this.fetchRegister(updatedRegister);
 	}
 
 	render(){
