b022b9c10340679d38b71760a0e49b79efd3ece2
Akshay199456 <akshay.kum94@gmail.com>
2019-02-14 19:38:58 -0800

    Completed Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './index.css';
 
 const Footer = () =>{
 	return(
 		<footer>
+			<div className='copyright'>
 			 Ⓒ 2019 Flutter
+			</div> 
+
+			<div className="github-links">
+				Follow us on GitHub:  <a className="link1" href="https://github.com/dawong8"> Danny Wong </a> 
+				<a href="https://github.com/Akshay199456" className="link2"> Akshay Mysore </a> 
+			</div>
 		</footer>
 	);
 }
